42d453d70f3804b308432411c0d6b6649c36d883
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-30 11:40:23 -0600

    Deconstruct arrays, and nested objects

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const animals = [
+  {
+    name: "cat",
+    sound: "meow",
+    feedingRequirments: {
+      food: 2,
+      water: 3,
+    },
+  },
+  { name: "dog", sound: "woof" },
+];
+export default animals;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import animals from "./data";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import reportWebVitals from "./reportWebVitals";
+
+//const animals = [
+//{
+//name: "cat",
+//sound: "meow",
+//feedingRequirments: {
+// food: 2,
+//water: 3,
+//},
+//},
+//{ name: "dog", sound: "woof" },
+//];
+//Desrtuctioning the whole array
+const [cat, dog] = animals;
+//console.log(animals);
+
+//Destructure properties of the cat object
+const {
+  name: catName,
+  sound: catSound,
+  feedingRequirments: catFeedingRequirments,
+} = cat;
+
+//console.log(catFeedingRequirments);
+
+// Destructuring properties of the dog object
+const { name: dogName, sound: dogSound, feedingRequirments = "Water" } = dog;
+
+console.log(feedingRequirments);
+
+const tesla = {
+  model: "Tesla Model S",
+  topSpeed: "200 mph",
+  topColour: "Red",
+};
+
+const honda = {
+  model: "Honda Civic",
+  topSpeed: "140 mph",
+  topColour: "Blue",
+};
+
+const App = () => (
   <React.StrictMode>
-    <App />
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+        <th>Top Colour</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{tesla.topSpeed}</td>
+        <td>{tesla.topColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{honda.topSpeed}</td>
+        <td>{honda.topColour}</td>
+      </tr>
+    </table>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
+
+reportWebVitals(console.log);
